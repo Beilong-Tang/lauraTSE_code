d320a605e19f711bc1504cae5c9a78d41f101042
beilong tang <bt132@duke.edu>
2025-04-17 12:37:47 +0000

    update

---
+++ recipes/run_tse_libri2mix_finetune.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+###########
+# Setting #
+###########
+
+name="libri2mix_finetune"
+config_path=exp/$name/config_log_mel_aux_5s_finetune_e_20.yaml
+resume=""
+export CUDA_VISIBLE_DEVICES="0,1,2,3"
+
+fine_tune="/DKUdata/tangbl/laura_TSE/ckpt/laura_tse_librispeech_dm_e_100.pth"
+
+. utils/parse_options.sh
+
+###############
+# DONT CHANGE #
+###############
+save_dir=$(dirname "$config_path")/$(basename "$config_path" .yaml)
+save_dir=${save_dir/#exp\//}
+echo $save_dir
+ckpt_path=ckpt/$save_dir
+log_path=log/$save_dir
+mkdir -p $ckpt_path
+mkdir -p $log_path
+
+###############
+## Run  DDP  ##
+###############
+python -u src/train.py --config $config_path --log $log_path --ckpt_path $ckpt_path --resume $resume \
+ --fine_tune $fine_tune
